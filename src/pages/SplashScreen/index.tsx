fcf122d9ce5159a4025d9a888b03e700ebd332fd
AngelieManueke <s22210219@student.unklab.ac.id>
2024-04-24 19:46:58 +0800

    SplashScreen

---
+++ src/pages/SplashScreen/index.tsx
@@ -0,0 +1,44 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import LogoPawP from '../../assets/icon/LogoPawP.svg';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('GetStarted');
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <LogoPawP style={styles.logo} />
+      <Text style={styles.text}>PAW PALACE</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E29C9C',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  logo: {
+    position: 'absolute',
+    top: '10%',
+    left: '10%',
+    fontSize: 100,
+    marginTop: 0,
+    marginLeft: -47,
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: 'Poppins-SemiBold',
+    color: '#E8D4CB',
+    marginTop: 50,
+  },
+});
